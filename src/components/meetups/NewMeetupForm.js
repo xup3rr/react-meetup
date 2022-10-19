08c14ae83997e64d38e8dd3a416e9b38a5726f14
xup3rr <xup3rr@gmail.com>
2022-10-19 10:33:21 +0200

    ✨ Add form funcionality
    -Handle form with react-hook-form
    -Submit form data to global state with NewMeetupAtom

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,34 +1,70 @@
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
+import { useAtom } from "jotai";
+import { NewMeetupAtom } from "../../store/MeetupAtom";
+import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
 
 export default function NewMeetupForm() {
-  function submitHandler(event) {
-    event.preventDefault();
-  }
+  const [, addMeetup] = useAtom(NewMeetupAtom);
+  const { register, handleSubmit, reset } = useForm();
+  const [formMessage, setFormMessage] = useState("");
+
+  const onSubmit = (data) => {
+    addMeetup(data);
+    reset();
+    setFormMessage("New Meetup added successfully");
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFormMessage("");
+    }, 1500);
+  }, [formMessage]);
 
   return (
     <Card>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input
+            type="text"
+            required
+            id="title"
+            {...register("title", { required: true })}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input
+            type="url"
+            required
+            id="image"
+            {...register("image", { required: true })}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input
+            type="text"
+            required
+            id="address"
+            {...register("address", { required: true })}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea
+            id="description"
+            rows="5"
+            {...register("description", { required: true })}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
         </div>
       </form>
+      {formMessage && <p>{formMessage}</p>}
     </Card>
   );
 }
