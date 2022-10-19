9ad19f57dc25e9c28a986cea8a173cd89d63536a
xup3rr <xup3rr@gmail.com>
2022-10-19 11:23:31 +0200

    🐛 Move FetchMeetupsAtom to AllMeetupPage
    -Fetch meetups and pass to MeetupList
    
    Strong relation with previous commit
    (b4e38366)

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { MeetupsAtom, FetchMeetupsAtom } from "../store/MeetupAtom";
 import MeetupList from "../components/meetups/MeetupList";
 
 export default function AllMeetupsPage() {
+  const [meetups] = useAtom(MeetupsAtom);
+  const [, fetchMeetups] = useAtom(FetchMeetupsAtom);
+
+  useEffect(() => {
+    if (!meetups) {
+      fetchMeetups("/data.json");
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <section>
-      <MeetupList />
+      <h1>All Meetups</h1>
+      <MeetupList meetups={meetups} />
     </section>
   );
 }
