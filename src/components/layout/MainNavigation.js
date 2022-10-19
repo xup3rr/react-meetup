cbd9ce4dcd2a6631d81b06b1cf9eac810ffe8986
xup3rr <xup3rr@gmail.com>
2022-10-19 12:23:00 +0200

    ✨ Add sticky Nav animation on scroll

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
-
+import { useState, useEffect } from "react";
 import { MeetupFavoriteAtom } from "../../store/MeetupFavoriteAtom";
 import { useAtom } from "jotai";
 
 export default function MainNavigation() {
   const [meetupFavorites] = useAtom(MeetupFavoriteAtom);
+  const [visible, setVisible] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+    setVisible(scrollPosition > position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrollPosition]);
 
   return (
-    <header className={classes.header}>
+    <header className={`${visible && classes.header}`}>
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
