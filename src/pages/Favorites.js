d3d8a817d108e850bb9b70460e11fa30a41fe61f
xup3rr <xup3rr@gmail.com>
2022-10-19 11:42:57 +0200

    ✨ Add MeetupsList to FavoritesPage
    -Render MeetupList with data from MeetupFavoriteAtom

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,14 @@
+import { useAtom } from "jotai";
+import { MeetupFavoriteAtom } from "../store/MeetupFavoriteAtom";
+import MeetupList from "../components/meetups/MeetupList";
+
 export default function FavoritesPage() {
+  const [meetups] = useAtom(MeetupFavoriteAtom);
+
   return (
     <section>
       <h1>Favorites Page</h1>
+      <MeetupList meetups={meetups} />
     </section>
   );
 }
